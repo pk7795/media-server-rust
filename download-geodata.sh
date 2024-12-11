818f4b6a241129ef4ab88a76c8b90bae5ccfd8c1
dk0xff <khoaphan7795@gmail.com>
2024-12-11 09:39:43 +0700

    update belo's cert

--- download-geodata.sh
+++ download-geodata.sh
@@ -1,4 +1,10 @@
+# mkdir -p maxminddb-data
+# cd maxminddb-data
+# rm -i GeoLite2-City.mmdb
+# wget https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb || { echo "Download GeoLite2-City database failed"; exit 1; }
+
+# run on mac
 mkdir -p maxminddb-data
 cd maxminddb-data
 rm -i GeoLite2-City.mmdb
-wget https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb || { echo "Download GeoLite2-City database failed"; exit 1; }
+curl -L -O https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb || { echo "Download GeoLite2-City database failed"; exit 1; }
